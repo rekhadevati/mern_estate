25f55113a1d4e3f5ad4bf0c2fe4c433660937197
Vinay Pattar <01fe22mca046@kletech.ac.in>
2024-03-30 13:50:22 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-blue-700">
+      <h1>Hello World!</h1>
+      <p>Welcome to the app.</p>
+    </div>
+  )
+}
+
+export default App
